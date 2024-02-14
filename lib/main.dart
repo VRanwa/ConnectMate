2de30faa6734d1080a891b2287703165c751dfda
Sagar <jokeeblack@gmail.com>
2024-02-14 19:24:22 +0530

    connect_mate started

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:connect_mate/src/screen/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => login_screen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 300,
+          width:  300,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/logo.PNG'),
+              fit: BoxFit.cover,
+            )
+          )
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/src/screen/splash/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:connect_mate/src/screen/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class splash_screen extends StatelessWidget {
+  const splash_screen({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => login_screen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+        ),
+      );
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.blue, // Set the background color to blue
+      body: Center(
+        child: Text(
+          'Splash Screen',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
