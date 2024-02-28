62220d459320c8c3eabccaf0634cdb3efe14019c
Aashish <kumar.ashish95b@gmail.com>
2024-02-28 17:29:47 +0530

    formed profile screen

--- lib/src/screen/profile/profile_screen.dart
+++ lib/src/screen/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:connect_mate/src/widgets/bottombar/custombottom_screen.dart';
 import 'package:flutter/material.dart';
 
 class profile_screen extends StatefulWidget {
@@ -12,7 +13,114 @@ class _profile_screenState extends State<profile_screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('profile'),
+        title: Text('Profile',style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(left: 20),
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(100),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 170,top: 20),
+              child: Text('Basic Details',style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(right: 260,),
+              child: Text('Name',style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ListTile(
+                    titleAlignment: ListTileTitleAlignment.center,
+                    trailing: Padding(
+                      padding: const EdgeInsets.all(17),
+                      child: Icon(Icons.navigate_next),
+                    ),
+                    title: Text("User Name"),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 260,),
+              child: Text('Phone No.',style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 10),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ListTile(
+                    titleAlignment: ListTileTitleAlignment.center,
+                    trailing: Padding(
+                      padding: const EdgeInsets.all(17),
+                      child: Icon(Icons.navigate_next),
+                    ),
+                    title: Text("Phone No."),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(right: 260,),
+              child: Text('Email id.',style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 10),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ListTile(
+                    titleAlignment: ListTileTitleAlignment.center,
+                    trailing: Padding(
+                      padding: const EdgeInsets.all(17),
+                      child: Icon(Icons.navigate_next),
+                    ),
+                    title: Text("Email id."),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+
+            CustomBottomBar()
+
+          ],
+        ),
       ),
     );
   }
